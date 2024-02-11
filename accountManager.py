b2dc408a03449ff165dde809ed52ec02473a0114
Sandeep Vangipuram <sandeep84@gmail.com>
2024-02-11 17:58:56 +0000

    Connect to account manager using piecash

---
+++ accountManager.py
@@ -0,0 +1,53 @@
+import piecash
+
+def populateAccountChildrenRecords(account, rootCurrency):
+    account_entry = {
+        "guid": account.guid,
+        "name": account.name,
+        "numChildren": len(account.children),
+        "children": list()
+    }
+
+    for child in account.children:
+        child_entry = {
+            "guid": child.guid,
+            "name": child.name,
+            "numChildren": len(child.children),
+        }
+
+        try:
+            child_entry['balance'] = float(child.get_balance(recurse=True))
+            child_entry['currency'] = child.commodity.mnemonic
+            child_entry['balanceGBP'] = float(child.get_balance(recurse=True, commodity=rootCurrency))
+        except Exception as e:
+            child_entry['balance'] = 0.0
+            child_entry['currency'] = "N/A"
+            child_entry['balanceGBP'] = 0.0
+            print(e)
+
+        account_entry['children'].append(child_entry)
+
+    return account_entry
+
+def getAccounts(account_url):
+    book = piecash.open_book(account_url, readonly=True)
+    root = book.root_account
+    rootCurrency = book.currencies[0]
+    accounts = populateAccountChildrenRecords(root, rootCurrency)
+    book.close()
+    return accounts
+
+def getAccount(account_url, **kwargs):
+    book = piecash.open_book(account_url, readonly=True)
+    root = book.root_account
+    rootCurrency = book.currencies[0]
+    account = populateAccountChildrenRecords(book.get(piecash.Account, **kwargs), rootCurrency)
+    book.close()
+    return account
+
+def shutdown():
+    pass
+
+if __name__ == "__main__":
+    print(getAccounts())
+    shutdown()
--- main.py
+++ main.py
@@ -3,27 +3,16 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "a13d64f8ad7882ab5ac575e14f0c9e4a6bf81731e3eadd2dc435db9800ea04b4"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+import json
+import accountManager
 
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
+with open("secrets.json", "r") as f:
+    secrets = json.loads(f.read())
 
 class Token(BaseModel):
     access_token: str
@@ -39,7 +28,7 @@ class User(BaseModel):
     email: str | None = None
     full_name: str | None = None
     disabled: bool | None = None
-
+    account_url: str
 
 class UserInDB(User):
     hashed_password: str
@@ -51,6 +40,18 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -82,7 +83,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, secrets["SECRET_KEY"], algorithm=secrets["ALGORITHM"])
     return encoded_jwt
 
 
@@ -93,14 +94,14 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, secrets["SECRET_KEY"], algorithms=[secrets["ALGORITHM"]])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user(secrets["users_db"], username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
@@ -118,14 +119,14 @@ async def get_current_active_user(
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ) -> Token:
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    user = authenticate_user(secrets["users_db"], form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=secrets["ACCESS_TOKEN_EXPIRE_MINUTES"])
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
@@ -144,3 +145,27 @@ async def read_own_items(
     current_user: Annotated[User, Depends(get_current_active_user)]
 ):
     return [{"item_id": "Foo", "owner": current_user.username}]
+
+@app.get("/accounts")
+def read_accounts(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return accountManager.getAccounts(current_user.account_url)
+
+@app.get("/accounts/by_name/{account_name}")
+def read_accounts_by_name(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    account_name: str = None
+):
+    return accountManager.getAccount(current_user.account_url, name=account_name)
+
+@app.get("/accounts/by_guid/{account_guid}")
+def read_accounts_by_guid(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    account_guid: str = None
+):
+    return accountManager.getAccount(current_user.account_url, guid=account_guid)
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    accountManager.shutdown()
